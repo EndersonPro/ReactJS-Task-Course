e8fffcc3f8c4c20410cbe32362dcf0e869d0bf09
Enderson Vizcaino <enjaviga@gmail.com>
2018-08-07 00:55:24 -0500

    Initial commit :tada:

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <nav className="navbar navbar-dark bg-dark">
+          <a href="" className="text-white">
+            Tasks
+          </a>
+        </nav>
+          <img src={logo} className="App-logo" alt="logo" />
+      </div>
+    );
+  }
+}
+
+export default App;
