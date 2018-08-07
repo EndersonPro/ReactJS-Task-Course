18105b282a4cbb0659c074b8b7e7073a242850e5
Enderson Vizcaino <enjaviga@gmail.com>
2018-08-07 02:07:36 -0500

    Todos

--- src/App.js
+++ src/App.js
@@ -2,15 +2,48 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Navigation from './components/Navigation';
+
+import { todos } from './todos.json'
+
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {todos}
+  }
+
   render() {
+    
+    const todos = this.state.todos.map((todo, i)=>{
+      return (
+        <div className="col-md-4">
+          <div className="card mt-4">
+            <div className="card-header">
+              <h3>{todo.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                {todo.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              <p>{todo.description}</p>
+              <p>{todo.responsible}</p>
+            </div>
+          </div>
+        </div>
+      )
+    })
+
     return (
       <div className="App">
-        <nav className="navbar navbar-dark bg-dark">
-          <a href="" className="text-white">
-            Tasks
-          </a>
-        </nav>
+          <Navigation len={this.state.todos.length}/>
+
+          <div className="container">
+            <div className="row mt-4">
+              { todos }
+            </div>
+          </div>
+
           <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+    render(){
+        return(
+            <nav className="navbar navbar-dark bg-dark">
+                <a href="" className="text-white">
+                    Taks
+
+                    <span className="badge badge-pill badge-light ml-2">
+                        {this.props.len}
+                    </span>
+                </a>
+            </nav>
+        )
+    }
+}
+
+export default Navigation;
